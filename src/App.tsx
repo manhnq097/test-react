85d417893da09b30a0b2b98699f07c72f7e60fc4
Manh Ngo <manhnq097@gmail.com>
2022-03-18 08:14:33 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { reverse } from "utils/function";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const testStr = reverse("Manh Ngo");
+
+const App: React.FC = () => {
+  return <div className="app">{testStr}</div>;
+};
 
 export default App;
